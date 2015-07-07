6a2c96ff3d52808bd95468f06c5f0d9e02b69930
zhjch05 <zjc1996@brandeis.edu>
2015-07-07 13:46:25 -0400

    init done version

--- client/pages/nlp.js
+++ client/pages/nlp.js
@@ -1,14 +1,17 @@
 NLP = function() {
+    //parsing functions
+    var getPieces = function(content) {
+        return content.match(/([a-h][1-8])|knight|bishop|queen|king|pawn|rook/g);
+    };
+    var getPreps = function(content) {
+        return content.match(/to|take|on|at|in/g);
+    };
+    var getDets = function(content) {
+        return content.match(/what|which|how|when|where/g);
+    };
+
+    //the environment object, main data structure
     var ENV = function() {
-        var getPieces = function(content) {
-            return content.match(/([a-h][1-8])|knight|bishop|queen|king|pawn|rook/g);
-        };
-        var getPreps = function(content) {
-            return content.match(/to|take|on|at|in/g);
-        };
-        var getDets = function(content) {
-            return content.match(/what|which|how|when|where/g);
-        }
         var Sentence = function(content, owner) {
             this.content = content;
             this.owner = owner;
@@ -18,14 +21,12 @@ NLP = function() {
             }
         }
         var dialogs = [];
-        var currentDialog = function() {
-            return dialogs.length - 1;
+        var getCurDialog = function(){
+            return dialogs[dialogs.length - 1];
         }
-        var currentSentence = function(curDialog) {
-            return dialogs[curDialog].length - 1;
-        }
-        var getCurrent = function() {
-            return dialogs[currentDialog()][currentSentence(currentDialog())];
+        var getCurSentence = function(){
+            curdialog = getCurDialog();
+            return curdialog[curdialog.length - 1];
         }
         return {
             init: function() {
@@ -33,20 +34,26 @@ NLP = function() {
                 var sentence = new Sentence('Hello from the NLP system. Input your command please. The instructions are on the left.', 'sys');
                 sentences.push(sentence);
                 dialogs.push(sentences);
-                return getCurrent();
+                return getCurSentence();
             }
         }
     };
+
+    //format the input
     var beautify = function(content) {
         content = content.trim()
             .toLowerCase()
-            .replace(/\s+/, ' ');
+            .replace(/\s+/g, ' ');
         return content;
     };
-    var move = function(source, target) {
+
+    //chess/chessboard control
+        var move = function(source, target) {
         onDrop(source, target);
         myboard.position(game.fen());
     };
+
+    //public API
     return {
         init: function() {
             env = new ENV();
