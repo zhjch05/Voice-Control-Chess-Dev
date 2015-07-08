50e35db759e454053602121930e4673e0b5669bd
zhjch05 <zjc1996@brandeis.edu>
2015-07-08 15:23:19 -0400

    move done

--- client/pages/nlp.js
+++ client/pages/nlp.js
@@ -1,14 +1,30 @@
 NLP = function() {
-
-
-    var Sentence = function(content, owner, pieces, preps, dets) {
+    var Sentence = function(content, owner, pieces, preps, dets, controlkey) {
         this.content = content;
         this.owner = owner;
         this.pieces = pieces;
         this.preps = preps;
         this.dets = dets;
-        var getIntent = function() {
-            return '';
+        this.controlkey = controlkey;
+        console.log({
+            content: this.content,
+            owner: this.owner,
+            pieces: this.pieces,
+            preps: this.preps,
+            dets: this.dets,
+            controlkey: this.controlkey,
+            intent: this.intent
+        });
+        if (owner === 'usr') {
+            if (controlkey !== null) {
+                this.intent = 'control';
+            } else if (dets !== null) {
+                this.intent = 'inquiry';
+            } else if ($.inArray('to', preps) > -1 || $.inArray('take', preps) > -1) {
+                this.intent = 'move';
+            } else if ($.inArray('at', preps) > -1 || $.inArray('on', preps) > -1 || $.inArray('in', preps) > -1) {
+                this.intent = 'inquiry';
+            } else this.intent = 'other';
         }
         return {
             content: this.content,
@@ -16,7 +32,8 @@ NLP = function() {
             pieces: this.pieces,
             preps: this.preps,
             dets: this.dets,
-            intent: getIntent()
+            controlkey: this.controlkey,
+            intent: this.intent
         }
     }
 
@@ -28,8 +45,11 @@ NLP = function() {
         return content.match(/to|take|on|at|in/g);
     };
     var getDets = function(content) {
-        return content.match(/what|which|how|when|where|can|could|may/g);
+        return content.match(/what|who|which|how|when|where|can|could|may/g);
     };
+    var getControlKey = function(content) {
+        return content.match(/reset|restart|undo|surrender/g);
+    }
 
     //the environment object, main data structure
     var ENV = function() {
@@ -44,7 +64,7 @@ NLP = function() {
         return {
             init: function() {
                 var sentences = [];
-                var sentence = new Sentence('Hello from the NLP system. Input your command please. The instructions are on the left.', 'sys', null, null, null);
+                var sentence = new Sentence('Hello from the NLP system. Input your command please. The instructions are on the left.', 'sys', null, null, null, null);
                 sentences.push(sentence);
                 dialogs.push(sentences);
                 return getCurSentence();
@@ -57,8 +77,10 @@ NLP = function() {
         var pieces = getPieces(content);
         var preps = getPreps(content);
         var dets = getDets(content);
-        var sentence = new Sentence(content, owner, pieces, preps, dets);
+        var controlkey = getControlKey(content);
+        var sentence = new Sentence(content, owner, pieces, preps, dets, controlkey);
         console.log(JSON.stringify(sentence));
+        return sentence;
     };
 
     //format the input
@@ -71,11 +93,14 @@ NLP = function() {
 
     //chess/chessboard control
     var move = function(source, target) {
-        onDrop(source, target);
+        if (source.search(/[a-h][1-8]/) > -1) {
+            onDrop(source, target);
+        }
+        else {
+            
+        }
         myboard.position(game.fen());
     };
-
-
 
     //public APIs
     return {
@@ -86,7 +111,49 @@ NLP = function() {
         input: function(content) {
             content = beautify(content);
             //content = errorCheck(content);
-            parseToSentence(content);
+            var sentence = parseToSentence(content);
+            if (sentence.intent !== null || sentence.intent !== undefined) {
+                switch (sentence.intent) {
+                    case 'control':
+
+                        break;
+                    case 'inquiry':
+
+                        break;
+                    case 'move':
+                        // corPieces = [];
+                        // _.each(sentence.pieces, function(piece) {
+                        //     if (piece.search(/[a-h][1-8]/) > -1) {
+                        //         corPieces.push(piece);
+                        //     }
+                        // });
+                        // switch (corPieces.length) {
+                        //     case 0:
+                        //         return -1;
+                        //         break;
+                        //     case 1:
+                        //
+                        //         break;
+                        //     case 1:
+                        //
+                        //         break;
+                        //     default:
+                        //         return -1;
+                        // }
+                        var pieces = sentence.pieces;
+                        if (pieces.length === 2) {
+                            move(pieces[0], pieces[1]);
+                            return 'Moved from ' + pieces[0] + ' to ' + pieces[1] + '.';
+                        }
+                        break;
+                    case 'other':
+
+                        break;
+                    default:
+                        console.log('bad switch intent');
+                        return;
+                }
+            }
         }
     }
 }
