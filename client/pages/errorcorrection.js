dd822e710d9c6a12ec19dc2774ae71e912f4299f
zhjch05 <zjc1996@brandeis.edu>
2015-07-16 10:23:02 -0400

    skele

---
+++ client/pages/errorcorrection.js
@@ -0,0 +1,31 @@
+errorCorrect = function(){
+    var wordProc = function(str){
+        var DwordProc = [
+
+        ];
+    };
+    var fragWordProc = function(str){
+
+    };
+    var comb = function(str){
+
+    };
+    var ngram = function(str){
+
+    };
+    var _5gram = function(str){
+
+    };
+    var _4gram = function(str){
+
+    }
+    var _3gram = function(str){
+
+    };
+    var _2gram = function(str){
+
+    };
+    var _1gram =  function(str){
+
+    };
+}
