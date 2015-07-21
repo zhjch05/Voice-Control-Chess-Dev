7e13459644a38e0ece32ed9fe6938e4dd166cb27
zhjch05 <zjc1996@brandeis.edu>
2015-07-21 10:49:35 -0400

    mcard

--- client/pages/matching.js
+++ client/pages/matching.js
@@ -1,3 +1,30 @@
 Template.matching.helpers({
-    desk: function(){return Matchings.find();}
+    desk: function() {
+        return Matchings.find();
+    }
 });
+
+Template.matching.events({
+    'click #createbtn': function() {
+        var exDesk = Matchings.findOne({
+            userId: Meteor.userId()
+        });
+        if (exDesk) {
+            alert('Already in desk!');
+        } else {
+            Meteor.call('createDesk', function(error, result) {
+                if (error) {
+                    return alert(error.reason);
+                }
+            });
+        }
+    }
+});
+
+Template.matching.rendered = function(){
+    Tracker.autorun(function (){
+        if (Matchings.findOne({userId: Meteor.userId()})){
+            $('#createbtn').toggleClass('disabled');
+        }
+    });
+}
--- server/startup.js
+++ server/startup.js
@@ -1,10 +1,10 @@
 Meteor.startup(function(){
-    if(Matchings.find().count() === 0){
-        Matchings.insert({host: "abc@abc.com", userId: "sssssssss"});
-        Matchings.insert({host: "abc1@abc.com", userId: "sssssssss"});
-        Matchings.insert({host: "abc2@abc.com", userId: "sssssssss"});
-        Matchings.insert({host: "abc3@abc.com", userId: "sssssssss"});
-        Matchings.insert({host: "abc4@abc.com", userId: "sssssssss"});
-        Matchings.insert({host: "abc5@abc.com", userId: "sssssssss"});
-    }
+    // if(Matchings.find().count() === 0){
+    //     Matchings.insert({host: "abc@abc.com", userId: "sssssssss"});
+    //     Matchings.insert({host: "abc1@abc.com", userId: "sssssssss"});
+    //     Matchings.insert({host: "abc2@abc.com", userId: "sssssssss"});
+    //     Matchings.insert({host: "abc3@abc.com", userId: "sssssssss"});
+    //     Matchings.insert({host: "abc4@abc.com", userId: "sssssssss"});
+    //     Matchings.insert({host: "abc5@abc.com", userId: "sssssssss"});
+    // }
 });
