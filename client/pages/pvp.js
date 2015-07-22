817736be11c8588ff08130aa8f1345d9b39ecd88
zhjch05 <zjc1996@brandeis.edu>
2015-07-21 23:42:27 -0400

    display user email for each game

--- client/pages/pvp.js
+++ client/pages/pvp.js
@@ -1,3 +1,11 @@
-Template.pvp.rendered = function(){
-    console.log(Router.current().params._id);
+Template.pvp.rendered = function() {
+    console.log(Router.current().params._id); //good
+    var post = Games.findOne({
+        _id: Router.current().params._id
+    });
+    console.log(post);
+    var playeremail1 = Meteor.users.findOne({
+        _id: post.player1
+    }).emails[0].address;
+    $('#youremail').append(playeremail1);
 }
