88384ae906f7059687d8584847736e9fb395c33c
zhjch05 <zjc1996@brandeis.edu>
2015-07-22 16:08:14 -0400

    sync

--- client/pages/pvp.js
+++ client/pages/pvp.js
@@ -4,8 +4,20 @@ Template.pvp.rendered = function() {
         _id: Router.current().params._id
     });
     console.log(post);
-    var playeremail1 = Meteor.users.findOne({
+    var player1 = Meteor.users.findOne({
         _id: post.player1
-    }).emails[0].address;
-    $('#youremail').append(playeremail1);
+    });
+    var player2 = Meteor.users.findOne({
+        _id: post.player2
+    });
+    console.log(Meteor.userId());
+    console.log(player1);
+    console.log(player2);
+    if (player1._id == Meteor.userId()) {
+        $('#youremail').append(player1.emails[0].address);
+        $('#oppemail').append(player2.emails[0].address);
+    } else {
+        $('#youremail').append(player2.emails[0].address);
+        $('#oppemail').append(player1.emails[0].address);
+    }
 }
