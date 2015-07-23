81dbb02a054d9bbcb831d90b93dc06a6675159a0
zhjch05 <zjc1996@brandeis.edu>
2015-07-23 14:41:19 -0400

    basic pvp done

--- client/pages/matching.js
+++ client/pages/matching.js
@@ -64,7 +64,7 @@ Template.mcard.events({
         var post = {
             player1: info.userId,
             player2: Meteor.userId(),
-            turn:'w',
+            startturn:'white',
             fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
         };
         //console.log(post);
