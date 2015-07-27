c1e669ae5f527533b7c5b2d4de51b67787f8ec2b
chyzhang@brandeis.edu <19940119zhch>
2015-07-27 14:54:49 -0400

    setting almost done

--- server/startup.js
+++ server/startup.js
@@ -3,18 +3,18 @@ Meteor.startup(function(){
         Fen.insert({flag:"only",fen:"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"});
     }
     if(personalTheme.find().count() === 0){
-        personalTheme.insert({personalTheme.insert({pieceName:"wP", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"wR", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"wN", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"wB", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"wQ", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"wK", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"bP", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"bR", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"bB", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"bN", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"bQ", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
-        personalTheme.insert({personalTheme.insert({pieceName:"bK", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});});
+        personalTheme.insert({pieceName:"wP", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"wR", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"wN", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"wB", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"wQ", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"wK", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"bP", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"bR", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"bB", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"bN", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"bQ", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
+        personalTheme.insert({pieceName:"bK", address:"'img/chesspieces/wikipedia/model1/{piece}.png'"});
         
     }
 });
